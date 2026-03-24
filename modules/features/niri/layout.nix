8b4522db4cb1853dc003e2163b3c4cada07e92e1
ParmaGianni <parmagianni@noreply.fj.jonsimeon.com>
2026-03-24 01:09:40 -0400

    fix: wrapped niri colors

--- modules/features/niri/layout.nix
+++ modules/features/niri/layout.nix
@@ -1,18 +1,26 @@
-{config, ...}: let
-  colors = config.lib.stylix.colors;
-  shadow = {
-    on = null;
-    softness = 30;
-    spread = 5;
-    offset.x = 0;
-    offset.y = 5;
-    color = "#${colors.base00}";
-  };
-in {
-  flake.aspects.niri.configs = {
+{
+  inputs,
+  moduleWithSystem,
+  ...
+}: {
+  flake.aspects.niri.config = moduleWithSystem ({pkgs, ...}: let
+    base16 = pkgs.callPackage (inputs.base16.lib) {};
+    colors = base16.mkSchemeAttrs "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+
+    shadow = {
+      on = null;
+      softness = 30;
+      spread = 5;
+      offset._attrs.x = 0;
+      offset._attrs.y = 5;
+      color = "${colors.withHashtag.base00}";
+    };
+  in {
     # layout
-    settings.layout = {
+    settings.layout = with colors.withHashtag; {
       inherit shadow;
+      border = {off = null;};
+      struts = null;
       gaps = 15;
       center-focused-column = "never";
       preset-column-widths = [
@@ -23,18 +31,16 @@ in {
       default-column-width.proportion = 0.5;
       focus-ring = {
         width = 1.5;
-        active-gradient.from = "#${colors.base0C}";
-        active-gradient.to = "#${colors.base0D}";
-        inactive-color = "#${colors.base04}";
-        border = {off = null;};
-        struts = null;
+        active-gradient._attrs.from = base0C;
+        active-gradient._attrs.to = base0D;
+        inactive-color = base04;
       };
     };
 
     # overview
-    settings.overview = {
-      inherit shadow;
-      backdrop-color = "#${colors.base02}";
+    settings.overview = with colors.withHashtag; {
+      workspace-shadow = shadow;
+      backdrop-color = base02;
     };
-  };
+  });
 }
